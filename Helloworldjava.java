64454439e2744225000aae876ac9d58ef4715a2e
Phillip <123585512+planpv@users.noreply.github.com>
2023-01-26 18:34:37 -0800

    Add files via upload

---
+++ Helloworldjava.java
@@ -0,0 +1,15 @@
+package com.mycompany.helloworldjava;
+
+/**
+ *
+ * @author Phillip Plangetis
+ * Date: 1/26/22
+ * Hello World Program
+ */
+
+public class Helloworldjava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
